a63fef65b9071605cceb31d7037d07bfca51e980
Slava <sik.pascal@gmail.com>
2022-05-29 22:13:45 +0200

    Finalize config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,30 @@
 module.exports = {
+  root: true,
   env: {
-    es2021: true,
+    es2020: true,
     node: true,
   },
   extends: [
     'airbnb-base',
+    'airbnb-typescript/base',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: '2020',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     '@typescript-eslint',
   ],
   rules: {
+    // '@typescript-eslint/no-unused-vars': 1,
+    // 'node/file-extension-in-import': 'never',
+    // 'import/extensions': ['error', 'ignorePackages', {
+    //   js: 'never',
+    //   ts: 'never',
+    //   mjs: 'never',
+    //   jsx: 'never',
+    // }],
   },
 };
