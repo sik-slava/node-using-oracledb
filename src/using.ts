30710199697f165223eddf305b0ab591ba303940
Slava <sik.pascal@gmail.com>
2022-05-29 22:14:33 +0200

    Add using.ts

---
+++ src/using.ts
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import * as db from 'oracledb';
+
+export type Connection = db.Connection;
+export type ConnectionAttributes = db.ConnectionAttributes;
+export type Pool = db.Pool;
+export type Connect = () => Promise<Connection>;
+export type Query<T> = (connection: Connection) => Promise<T>;
+
+export async function using<T>(attributes: ConnectionAttributes, query: Query<T>): Promise<T>;
+export async function using<T>(connect: Connect, query: Query<T>): Promise<T>;
+export async function using<T>(pool: Pool, query: Query<T>): Promise<T>;
+
+export async function using<T>(
+  param: Connect | Pool | ConnectionAttributes,
+  query: Query<T>,
+): Promise<T> {
+  const resolve = async (): Promise<Connection> => {
+    if ('connectionsInUse' in param) {
+      return param.getConnection();
+    }
+
+    if (!(param instanceof Function)) {
+      return db.getConnection(param);
+    }
+
+    return param();
+  };
+
+  const release = (conn: Connection) => async () => {
+    await conn.close()
+      .catch(() => {});
+  };
+
+  const connection = await resolve();
+
+  return query(connection)
+    .finally(release(connection));
+}
